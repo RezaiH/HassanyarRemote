f7015ee5d39ec6fc6d9f65482412c6f16932ef57
Hussain Rezai <rezaih637@gmail.com>
2022-11-15 15:25:43 -0500

    third commit

--- src/first/Commit.java
+++ src/first/Commit.java
@@ -5,4 +5,8 @@ public class Commit {
 	public static void main(String[] args) {
 		System.out.println("Hi, this is the first commit!");
 	}
+	
+	public static void showRemote() {
+		System.out.println("Remote repository is not yet created!");
+	}
 }
